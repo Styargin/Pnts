6aa53f0c224af6e354409102d964e4db5ca913f2
Isaak Off <offst.ionov@gmail.com>
2020-07-21 01:27:57 +0500

    Сторона сервера, для прослушивания на визическом компьютере

--- RvrsshllSrv.py
+++ RvrsshllSrv.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # For TCP use SOCK_STREAM
-s.bind(('127.0.0.1', 8888))
+s.bind(('0.0.0.0', 8888))  # Default s.bind(('127.0.0.1', 8888))
 result = s.recv(1024)
 print('Message:', result.decode('utf-8'))
 s.close()
