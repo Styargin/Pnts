e5936214c62ecfcb5c95e529a82b9fb7b5fed9b9
Isaak Off <offst.ionov@gmail.com>
2020-07-21 01:32:58 +0500

    Сторона клиента, с IP адресом физического компьютера

--- RvrssshllCln.py
+++ RvrssshllCln.py
@@ -1,4 +1,4 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # For TCP use SOCK_STREAM
-s.sendto(b'<Your message>', ('127.0.0.1', 8888))
+s.sendto(b'<Your message>', ('192.168.0.1', 8888))  # Default s.sendto(b'<Your message>', ('127.0.0.1', 8888))
