ef499223fac5a78965b6ec24d8efc9594b53ef45
Isaak Off <offst.ionov@gmail.com>
2020-07-21 20:08:52 +0500

    Сторона клиента TCP

--- RvrssshllCln.py
+++ RvrssshllCln.py
@@ -1,4 +1,13 @@
+# import socket
+#
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # For TCP use SOCK_STREAM
+# s.sendto(b'<Your message>', ('192.168.0.1', 8888))  # Default s.sendto(b'<Your message>', ('127.0.0.1', 8888))
+
+# Ispol'zuem TCP
+
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # For TCP use SOCK_STREAM
-s.sendto(b'<Your message>', ('192.168.0.1', 8888))  # Default s.sendto(b'<Your message>', ('127.0.0.1', 8888))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 8888))
+s.send(b'<YOUR MESSAGE>')
+s.close()
