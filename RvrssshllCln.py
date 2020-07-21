ea0395e3c261d0ed71fd7f167523ba329eb786ce
Isaak Off <offst.ionov@gmail.com>
2020-07-21 22:22:59 +0500

    Параметры для физических машин

--- RvrssshllCln.py
+++ RvrssshllCln.py
@@ -8,6 +8,6 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 8888))
+s.connect(('192.168.0.11', 8888))  # Default s.connect(('127.0.0.1', 8888))
 s.send(b'<YOUR MESSAGE>')
 s.close()
