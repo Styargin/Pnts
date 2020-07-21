c13311588d5e2b19b0dc16ca30d8f0e2345d7828
Isaak Off <offst.ionov@gmail.com>
2020-07-21 20:04:34 +0500

    Сторона сервера TCP

--- RvrsshllSrv.py
+++ RvrsshllSrv.py
@@ -1,7 +1,24 @@
+# import socket
+#
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # For TCP use SOCK_STREAM
+# s.bind(('0.0.0.0', 8888))  # Default s.bind(('127.0.0.1', 8888))
+# result = s.recv(1024)
+# print('Message:', result.decode('utf-8'))
+# s.close()
+
+# Ispol'zuem TCP
+
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # For TCP use SOCK_STREAM
-s.bind(('0.0.0.0', 8888))  # Default s.bind(('127.0.0.1', 8888))
-result = s.recv(1024)
-print('Message:', result.decode('utf-8'))
-s.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 8888))
+s.listen(5)
+while 1:
+    try:
+        client, addr = s.accept()
+    except KeyboardInterrupt:
+        s.close()
+        break
+    else:
+        result = client.recv(1024)
+        print('Message:', result.decode('utf-8'))
